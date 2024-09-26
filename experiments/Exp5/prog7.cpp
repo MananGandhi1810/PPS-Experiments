3da78e75aa156e6e56e42c03e5858196faf37531
Manan Gandhi <ardumanan@gmail.com>
2024-09-26 15:40:42 +0530

    Some changes

--- experiments/Exp5/prog7.cpp
+++ experiments/Exp5/prog7.cpp
@@ -5,7 +5,7 @@
  * *
 *   * 
 */
-#include <iostream>
+#include <iostream> 
 
 using namespace std;
 int main()
--- experiments/Exp6/prog1.cpp
+++ experiments/Exp6/prog1.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 int main()
 {
-  int n;
-  cout << "Enter n: ";
-  cin >> n;
-  int arr[n];
-  cout << "Enter elements: " << endl;
-  for (int i = 0; i < n; i++)
-  {
-    cout << "Element " << i + 1 << ": ";
-    cin >> arr[i];
-  }
-  for (int i = 0; i < n; i++)
-  {
-    arr[i] *= 5;
-  }
-  for (int i = 0; i < n; i++)
-  {
-    cout << arr[i] << endl;
-  }
-  return 0;
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] *= 5;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
 }
